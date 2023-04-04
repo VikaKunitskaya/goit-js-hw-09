b2179de47e13863444e69c123fd68379c68fd26b
VikaKunitskaya <116502841+VikaKunitskaya@users.noreply.github.com>
2023-04-04 14:58:41 +0000

    Deploying to gh-pages from @ VikaKunitskaya/goit-js-hw-09@ab27962143e70359226613d6540c3c6bd5f9148c 🚀

---
+++ 01-color-switcher.ac36f7f0.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.body};t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,timerId=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.ac36f7f0.js.map
